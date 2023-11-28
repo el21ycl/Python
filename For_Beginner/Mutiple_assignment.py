1d9b2696ffced6857c9f0e97b9c431713108b4f7
el21ycl <98757550+el21ycl@users.noreply.github.com>
2023-11-28 15:11:41 +0000

    String methods

---
+++ For_Beginner/Mutiple_assignment.py
@@ -0,0 +1,15 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+name = "Lin"
+age = 23
+attractive = True
+# Use one line of code
+name, age, attractive = "Lin",23,True
+print(name,age,attractive)
+
+spongebob = 30
+patrick = 30
+sandy = 30
+squidward = 30
+# Use one line of code
+squidward = patrick = sandy = squidward = 30
+print(spongebob,patrick,sandy,squidward)
---
+++ For_Beginner/string_methods.py
@@ -0,0 +1,29 @@
+
+name = "bro code" # 8, including blank space
+# How long the length of our string
+print(len(name))
+
+# How to find a character within a string
+print(name.find("o")) # is index 2
+
+# To capitalize that only the first letter 
+print(name.capitalize()) # Bro code
+
+# Make your string all uppercase/lowercase
+print(name.upper())
+print(name.lower())
+
+# isdigit will return True or False, depending on if our string is a digit or not
+print(name.isdigit()) # name is a string,so is False, unless name = 123
+
+# isalpha is check to see If your string contains only letters, alphabetical letters
+print(name.isalpha()) # if it have space, it is False. When name="Brocode", that is True
+
+# To count how many o's in here
+print(name.count("o")) # Bro code has two o, so return two
+
+# Replace all of o to a
+print(name.replace("o","a"))
+
+# Print 3 times
+print(name*3)
