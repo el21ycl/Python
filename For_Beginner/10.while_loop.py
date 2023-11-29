0afb51d9bd5a761c1b62cfeacc5f09d5f1cbd375
el21ycl <98757550+el21ycl@users.noreply.github.com>
2023-11-29 17:00:11 +0000

    how to use while loop

---
+++ For_Beginner/10.while_loop.py
@@ -0,0 +1,12 @@
+# while loop = a statement that will execute it's block of code,
+#              as long as it's condition remains true
+
+#while 1==1: it will just continue on and on
+
+name = ""
+
+while len(name) == 0:
+    name = input("Enter your name: ")
+
+print("Hello "+name)
+
