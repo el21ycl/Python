931d056807a7c6a52fdd4e53942cc4a724dd8d91
el21ycl <98757550+el21ycl@users.noreply.github.com>
2023-12-01 00:32:24 +0000

    2D list
    print([][])
    column row

---
+++ For_Beginner/15.2D_list.py
@@ -0,0 +1,11 @@
+# 2D lists = a list of lists
+
+drinks = "coffee","soda","tea"
+dinner = ["pizza","hamburger","hotdog"]
+dessert = ["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+print(drinks)
+print(food)
+
+
