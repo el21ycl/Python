a9448e7948a08524f764282790e0bc2ed0a8eb01
el21ycl <98757550+el21ycl@users.noreply.github.com>
2023-12-03 23:49:04 +0000

    index operator

---
+++ For_Beginner/19.indexing.py
@@ -0,0 +1,21 @@
+# index operator [] = gives access to a sequence's element (str,list,tuples)
+
+name = "bro Code!"
+
+if(name[0].islower()): #True
+    name = name.capitalize()
+
+first_name = name[:3].upper()
+last_name = name[4:].lower()
+last_character = name[-2]
+
+print(name)
+
+print(first_name)
+print(last_name)
+print(last_character)
+
+
+
+
+
