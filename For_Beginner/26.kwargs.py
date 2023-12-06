9e03c20c0db39c9b6c65e3bef07bdf7d0787a39e
el21ycl <98757550+el21ycl@users.noreply.github.com>
2023-12-06 23:29:21 +0000

    Update 26.kwargs.py

--- For_Beginner/26.kwargs.py
+++ For_Beginner/26.kwargs.py
@@ -9,7 +9,7 @@ hello(first="Bro",last="Code")
 
 # use two asterisk then quarks
 def hello(**names):
-    #print("Hello "+kwargs["first"]+" "+kwargs['last'])
+    #print("Hello "+ names["first"]+" "+ names['last'])
     print("Hello",end=" ")
     for key,value in names.items():
         print(value,end=" ")
