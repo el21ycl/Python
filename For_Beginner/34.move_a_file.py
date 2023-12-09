148c808ca6b9cbc822c9b793b84c335fe4b516e5
el21ycl <98757550+el21ycl@users.noreply.github.com>
2023-12-09 01:25:50 +0000

    Update 34.move_a_file.py

--- For_Beginner/34.move_a_file.py
+++ For_Beginner/34.move_a_file.py
@@ -1,13 +1,13 @@
 import os
 
 source = r'C:\Users\lin\Documents\GitHub\el21ycl\Workspace\Python\test.txt'
-destination = r'C:\Users\lin\Desktop\test.txt'
+destination = r'C:\Users\lin\Desktop\test2.txt'
 
 try:
     if os.path.exists(destination):
         print("There is already a file there")
     else:
-        os.replace(source,destination)
+        os.replace(source,destination) # that is how to move a file and or directory
         print(source +  " was moved")
 
 except FileExistsError:
